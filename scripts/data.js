a5193bcad208a34e420218cff20f9819a465afcd
Victor-official <vyk95@ukr.net>
2020-09-29 12:20:06 +0400

    first commit

---
+++ scripts/data.js
@@ -0,0 +1,581 @@
+export default [
+  {
+    id: 5589514,
+    pageURL:
+      "https://pixabay.com/photos/rose-pink-rose-flower-pink-flower-5589514/",
+    type: "photo",
+    tags: "rose, pink, flower",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/21/09/33/rose-5589514_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dd4a4f53a814f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dd4a4f53a814f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 3000,
+    imageHeight: 2000,
+    imageSize: 721561,
+    views: 10536,
+    downloads: 9961,
+    favorites: 21,
+    likes: 68,
+    comments: 92,
+    user_id: 12752456,
+    user: "Mammiya",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/27/19-31-11-716_250x250.jpeg",
+  },
+  {
+    id: 5595483,
+    pageURL:
+      "https://pixabay.com/photos/orange-flower-flower-petals-bloom-5595483/",
+    type: "photo",
+    tags: "orange, flower, petals",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/23/09/12/orange-flower-5595483_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dc464e5aaf14f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dc464e5aaf14f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 5184,
+    imageHeight: 3456,
+    imageSize: 2182442,
+    views: 11021,
+    downloads: 8825,
+    favorites: 34,
+    likes: 60,
+    comments: 20,
+    user_id: 932379,
+    user: "KRiemer",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/07/07/05-34-37-853_250x250.jpg",
+  },
+  {
+    id: 5591270,
+    pageURL:
+      "https://pixabay.com/photos/landscape-tree-bench-park-meadow-5591270/",
+    type: "photo",
+    tags: "landscape, tree, bench",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/21/21/24/landscape-5591270_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dc424855ac14f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dc424855ac14f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 5184,
+    imageHeight: 3456,
+    imageSize: 3966463,
+    views: 19982,
+    downloads: 17928,
+    favorites: 42,
+    likes: 68,
+    comments: 27,
+    user_id: 6795508,
+    user: "fietzfotos",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2017/10/24/21-58-51-377_250x250.jpg",
+  },
+  {
+    id: 5581792,
+    pageURL:
+      "https://pixabay.com/photos/lynx-animal-predator-european-lynx-5581792/",
+    type: "photo",
+    tags: "lynx, animal, predator",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/18/13/05/lynx-5581792_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dd424d5bae14f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dd424d5bae14f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 6240,
+    imageHeight: 4160,
+    imageSize: 5961419,
+    views: 23385,
+    downloads: 21125,
+    favorites: 41,
+    likes: 98,
+    comments: 98,
+    user_id: 10327513,
+    user: "NickyPe",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/23/17-02-11-399_250x250.jpg",
+  },
+  {
+    id: 5599141,
+    pageURL:
+      "https://pixabay.com/photos/roosters-hens-chickens-chicken-coop-5599141/",
+    type: "photo",
+    tags: "roosters, hens, chickens",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/24/16/12/roosters-5599141_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dc4a4b56ad14f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dc4a4b56ad14f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 7087,
+    imageHeight: 4724,
+    imageSize: 12796253,
+    views: 4151,
+    downloads: 3877,
+    favorites: 19,
+    likes: 50,
+    comments: 50,
+    user_id: 3764790,
+    user: "enriquelopezgarre",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/01/06-45-36-722_250x250.jpg",
+  },
+  {
+    id: 5600214,
+    pageURL:
+      "https://pixabay.com/photos/dahlia-flower-yellow-flower-petals-5600214/",
+    type: "photo",
+    tags: "dahlia, flower, yellow",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/25/00/44/dahlia-5600214_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e6d5434853a814f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e6d5434853a814f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 5916,
+    imageHeight: 3944,
+    imageSize: 3724679,
+    views: 3586,
+    downloads: 3451,
+    favorites: 12,
+    likes: 43,
+    comments: 36,
+    user_id: 1767157,
+    user: "Capri23auto",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/13/08-31-34-23_250x250.jpg",
+  },
+  {
+    id: 5596669,
+    pageURL:
+      "https://pixabay.com/photos/hedgehog-animal-pet-spiny-mammal-5596669/",
+    type: "photo",
+    tags: "hedgehog, animal, pet",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/23/18/04/hedgehog-5596669_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dc454c54a514f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dc454c54a514f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 6000,
+    imageHeight: 4000,
+    imageSize: 3959406,
+    views: 11038,
+    downloads: 10569,
+    favorites: 17,
+    likes: 65,
+    comments: 57,
+    user_id: 1767157,
+    user: "Capri23auto",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/13/08-31-34-23_250x250.jpg",
+  },
+  {
+    id: 5598268,
+    pageURL:
+      "https://pixabay.com/photos/crow-black-bird-hooded-crow-bill-5598268/",
+    type: "photo",
+    tags: "crow, black, bird",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/24/10/24/crow-5598268_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dc4b4854a414f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dc4b4854a414f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 6000,
+    imageHeight: 4000,
+    imageSize: 3331538,
+    views: 1722,
+    downloads: 1619,
+    favorites: 7,
+    likes: 48,
+    comments: 50,
+    user_id: 12696704,
+    user: "papazachariasa",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2019/06/06/14-40-36-354_250x250.jpg",
+  },
+  {
+    id: 5589532,
+    pageURL:
+      "https://pixabay.com/photos/rose-pink-rose-flower-pink-petals-5589532/",
+    type: "photo",
+    tags: "rose, pink, flower",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/21/09/35/rose-5589532_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dd4a4f51ae14f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dd4a4f51ae14f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 4454,
+    imageHeight: 2969,
+    imageSize: 1322113,
+    views: 6698,
+    downloads: 6502,
+    favorites: 12,
+    likes: 50,
+    comments: 73,
+    user_id: 12752456,
+    user: "Mammiya",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/27/19-31-11-716_250x250.jpeg",
+  },
+  {
+    id: 5600213,
+    pageURL:
+      "https://pixabay.com/photos/dahlia-flower-plant-petals-blossom-5600213/",
+    type: "photo",
+    tags: "dahlia, flower, plant",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/25/00/43/dahlia-5600213_150.jpg",
+    previewWidth: 150,
+    previewHeight: 99,
+    webformatURL:
+      "https://pixabay.com/get/53e6d5434853af14f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 423,
+    largeImageURL:
+      "https://pixabay.com/get/53e6d5434853af14f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 5965,
+    imageHeight: 3944,
+    imageSize: 3780463,
+    views: 1661,
+    downloads: 1549,
+    favorites: 12,
+    likes: 40,
+    comments: 36,
+    user_id: 1767157,
+    user: "Capri23auto",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/13/08-31-34-23_250x250.jpg",
+  },
+  {
+    id: 5590487,
+    pageURL:
+      "https://pixabay.com/photos/seascape-storm-lightning-sea-ocean-5590487/",
+    type: "photo",
+    tags: "seascape, storm, lightning",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/21/16/08/seascape-5590487_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dc434e5aab14f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dc434e5aab14f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 7087,
+    imageHeight: 4724,
+    imageSize: 4782789,
+    views: 16631,
+    downloads: 15958,
+    favorites: 30,
+    likes: 65,
+    comments: 61,
+    user_id: 3764790,
+    user: "enriquelopezgarre",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/01/06-45-36-722_250x250.jpg",
+  },
+  {
+    id: 5592320,
+    pageURL:
+      "https://pixabay.com/photos/jellyfish-sea-jellies-animals-5592320/",
+    type: "photo",
+    tags: "jellyfish, sea, animals",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/22/08/27/jellyfish-5592320_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dc414950ac14f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dc414950ac14f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 7360,
+    imageHeight: 4912,
+    imageSize: 8011307,
+    views: 8930,
+    downloads: 4602,
+    favorites: 40,
+    likes: 48,
+    comments: 23,
+    user_id: 59194,
+    user: "djedj",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2018/07/07/23-27-51-148_250x250.jpg",
+  },
+  {
+    id: 5589515,
+    pageURL:
+      "https://pixabay.com/photos/rose-pink-rose-flower-pink-flower-5589515/",
+    type: "photo",
+    tags: "rose, pink, flower",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/21/09/33/rose-5589515_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dd4a4f53a914f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dd4a4f53a914f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 3629,
+    imageHeight: 2419,
+    imageSize: 1187382,
+    views: 2329,
+    downloads: 2179,
+    favorites: 12,
+    likes: 47,
+    comments: 70,
+    user_id: 12752456,
+    user: "Mammiya",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/27/19-31-11-716_250x250.jpeg",
+  },
+  {
+    id: 5586385,
+    pageURL:
+      "https://pixabay.com/photos/body-spirit-fire-smoke-sunset-5586385/",
+    type: "photo",
+    tags: "body, spirit, fire",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/20/08/28/body-5586385_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dd45495aa914f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dd45495aa914f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 4211,
+    imageHeight: 2807,
+    imageSize: 3723518,
+    views: 14650,
+    downloads: 13521,
+    favorites: 38,
+    likes: 77,
+    comments: 49,
+    user_id: 13452116,
+    user: "Syaibatulhamdi",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/08/19/09-44-13-930_250x250.png",
+  },
+  {
+    id: 5586798,
+    pageURL:
+      "https://pixabay.com/photos/fly-insect-dahlia-nature-flower-5586798/",
+    type: "photo",
+    tags: "fly, insect, dahlia",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/20/11/12/fly-5586798_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dd454d5ba414f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dd454d5ba414f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 5330,
+    imageHeight: 3553,
+    imageSize: 1869687,
+    views: 8120,
+    downloads: 5715,
+    favorites: 12,
+    likes: 62,
+    comments: 58,
+    user_id: 10084616,
+    user: "Nennieinszweidrei",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/16/19-22-39-462_250x250.jpg",
+  },
+  {
+    id: 5584820,
+    pageURL:
+      "https://pixabay.com/photos/townscape-cityscape-town-city-5584820/",
+    type: "photo",
+    tags: "townscape, cityscape, town",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/19/15/43/townscape-5584820_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dd474250ac14f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 426,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dd474250ac14f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 6048,
+    imageHeight: 4024,
+    imageSize: 9582230,
+    views: 24232,
+    downloads: 19670,
+    favorites: 28,
+    likes: 78,
+    comments: 62,
+    user_id: 1032521,
+    user: "Tama66",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/19/17-00-44-173_250x250.jpeg",
+  },
+  {
+    id: 5596668,
+    pageURL:
+      "https://pixabay.com/photos/hoverfly-fly-insect-flower-fly-5596668/",
+    type: "photo",
+    tags: "hoverfly, fly, insect",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/23/18/03/hoverfly-5596668_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5dc454c54a414f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dc454c54a414f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 6000,
+    imageHeight: 4000,
+    imageSize: 3046961,
+    views: 6816,
+    downloads: 6628,
+    favorites: 13,
+    likes: 53,
+    comments: 46,
+    user_id: 1767157,
+    user: "Capri23auto",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/13/08-31-34-23_250x250.jpg",
+  },
+  {
+    id: 5584131,
+    pageURL: "https://pixabay.com/photos/forest-princess-cinderella-5584131/",
+    type: "photo",
+    tags: "forest, princess, cinderella",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/19/10/39/forest-5584131_150.jpg",
+    previewWidth: 129,
+    previewHeight: 150,
+    webformatURL:
+      "https://pixabay.com/get/53e5dd474b51ad14f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 549,
+    webformatHeight: 640,
+    largeImageURL:
+      "https://pixabay.com/get/53e5dd474b51ad14f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 2872,
+    imageHeight: 3350,
+    imageSize: 3342287,
+    views: 17448,
+    downloads: 16468,
+    favorites: 43,
+    likes: 81,
+    comments: 68,
+    user_id: 13452116,
+    user: "Syaibatulhamdi",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/08/19/09-44-13-930_250x250.png",
+  },
+  {
+    id: 5563684,
+    pageURL:
+      "https://pixabay.com/photos/landscape-cave-moon-twilight-night-5563684/",
+    type: "photo",
+    tags: "landscape, cave, moon",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/11/17/01/landscape-5563684_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e5d3404c5aa814f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 427,
+    largeImageURL:
+      "https://pixabay.com/get/53e5d3404c5aa814f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 5863,
+    imageHeight: 3909,
+    imageSize: 6155667,
+    views: 50512,
+    downloads: 46081,
+    favorites: 73,
+    likes: 148,
+    comments: 94,
+    user_id: 3764790,
+    user: "enriquelopezgarre",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/01/06-45-36-722_250x250.jpg",
+  },
+  {
+    id: 5602429,
+    pageURL:
+      "https://pixabay.com/photos/doll-face-woman-person-female-5602429/",
+    type: "photo",
+    tags: "doll, face, woman",
+    previewURL:
+      "https://cdn.pixabay.com/photo/2020/09/25/19/23/doll-5602429_150.jpg",
+    previewWidth: 150,
+    previewHeight: 100,
+    webformatURL:
+      "https://pixabay.com/get/53e6d5414e50a514f1dc846096293f7a1039dee0564c704f752e79dc904bc550_640.jpg",
+    webformatWidth: 640,
+    webformatHeight: 425,
+    largeImageURL:
+      "https://pixabay.com/get/53e6d5414e50a514f6da8c7dda793676113ad8e556546c48732f7ad69f4ac358b0_1280.jpg",
+    imageWidth: 6000,
+    imageHeight: 3989,
+    imageSize: 2846558,
+    views: 914,
+    downloads: 837,
+    favorites: 20,
+    likes: 20,
+    comments: 2,
+    user_id: 686414,
+    user: "Alexas_Fotos",
+    userImageURL:
+      "https://cdn.pixabay.com/user/2020/09/12/06-58-33-197_250x250.jpg",
+  },
+];
---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+import data from "./data.js";
+
+var template = $("#flowerTmpl");
+template.tmpl(data).appendTo(".js-list");
+
+var cards = $(".list__item");
+var tagsArray = [];
+var bigTagsList = $(".js-tag-list");
+var tagElement = $(".tag-list__item");
+
+tagElement.click(tagElementClickHandler);
+
+function tagElementClickHandler(e) {
+  var tagText = $(e.target).text();
+
+  cards
+    .hide()
+    .filter("." + tagText)
+    .show();
+}
+
+cards.each(function (index) {
+  var cardTagsArr = $(this).attr("class").split(" ");
+
+  for (var i = 0; i < cardTagsArr.length; i++) {
+    if (
+      cardTagsArr[i] != "list__item" &&
+      tagsArray.indexOf(cardTagsArr[i]) < 0
+    ) {
+      tagsArray.push(cardTagsArr[i]);
+    }
+  }
+});
+
+$(tagsArray).each(function (index) {
+  //clone any tagElement with the event
+  $(tagElement[0]).clone(true).text(tagsArray[index]).appendTo(bigTagsList);
+});
